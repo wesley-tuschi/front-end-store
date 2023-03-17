cd837161606494907a004e7c9d7e8aba4f9c5fda
Gabriel Campos <101124404+gabrielcampostrybe@users.noreply.github.com>
2023-03-17 15:37:17 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
-  const fetchResult = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+  const fetchResult = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const data = fetchResult.json();
   return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const fetchResult = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+  const fetchResult = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const data = fetchResult.json();
   return data;
 }
