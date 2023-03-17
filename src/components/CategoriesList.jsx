aeb5d6777588953be32d9567eb8962e5a767e369
Gabriel Matsuda <gabrielomoto17@gmail.com>
2023-03-17 18:51:23 -0300

    lista de categorias req4

---
+++ src/components/CategoriesList.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getCategories } from '../services/api';
+
+class CategoriesList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    getCategories().then((data) => this.setState({ categories: data }));
+  }
+
+  render() {
+    const { categories } = this.state;
+    const { selectCategory } = this.props;
+    return (
+      <>
+        <p>Categorias</p>
+        {
+          categories.map((category) => (
+            <button
+              key={ category.id }
+              data-testid="category"
+              type="button"
+              onClick={ () => selectCategory(category.id) }
+              value={ category.name }
+            >
+              { category.name }
+            </button>
+          ))
+        }
+      </>
+    );
+  }
+}
+
+CategoriesList.propTypes = {
+  selectCategory: PropTypes.func,
+};
+
+CategoriesList.defaultProps = {
+  selectCategory: () => '',
+};
+
+export default CategoriesList;
