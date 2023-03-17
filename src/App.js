2bf95d20207d0c110d5bbeef13685e9db4f59609
gabrielcampos <gabrielcampos78@gmail.com>
2023-03-17 17:29:49 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
+import shoppingCart from './img/shoppingCart.png';
+import Cart from './pages/cart/Cart';
 import ProductList from './pages/ProductList';
 
 class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={ ProductList } />
-      </Switch>
+      <main>
+        <Link data-testid="shopping-cart-button" to="/cart">
+          <img height="20" src={ shoppingCart } alt="Shopping Cart" />
+        </Link>
+        <Switch>
+          <Route path="/" exact component={ ProductList } />
+          <Route path="/cart" component={ Cart } />
+        </Switch>
+      </main>
     );
   }
 }
